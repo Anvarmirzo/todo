021fd11352568fa2de692469d07e3b605f7ee9b5
Anvarmirzo <www.latvienhoasadrizeges@gmail.com>
2022-05-14 15:42:30 +0500

    added detecting platforms

--- src/components/ui/AppButton.js
+++ src/components/ui/AppButton.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {View, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, StyleSheet, TouchableOpacity, Platform, TouchableNativeFeedback} from 'react-native';
 import {THEME} from '../../theme';
 import {AppTextBold} from './AppTextBold';
 
 export const AppButton = ({children, onPress, color = THEME.MAIN_COLOR}) => {
+	const Wrapper = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
+
 	return (
-		<TouchableOpacity onPress={onPress} activeOpacity={0.7}>
+		<Wrapper onPress={onPress} activeOpacity={0.7}>
 			<View style={{...styles.button, backgroundColor: color}}>
 				<AppTextBold style={styles.text}>{children}</AppTextBold>
 			</View>
-		</TouchableOpacity>
+		</Wrapper>
 	);
 };
 
